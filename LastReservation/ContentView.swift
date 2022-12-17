0969efb9f83c21e484609dc3acf502af40f97754
Berat Yavuz <beratttyavuzz@gmail.com>
2022-12-17 13:55:49 +0300

    Initial Commit

---
+++ LastReservation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LastReservation
+//
+//  Created by Berat Yavuz on 17.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LastReservation/LastReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastReservationApp.swift
+//  LastReservation
+//
+//  Created by Berat Yavuz on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LastReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
