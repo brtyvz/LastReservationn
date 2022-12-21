70d9aa335a2bcf25da486ee11f5e01f0c7d8f9de
Berat Yavuz <beratttyavuzz@gmail.com>
2022-12-21 23:38:02 +0300

    Tab bar menu added

---
+++ LastReservation/Core/Main/MainMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuView.swift
+//  LastReservation
+//
+//  Created by Berat Yavuz on 21.12.2022.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text("Main Menu!")
+    }
+}
+
+struct MainMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
---
+++ LastReservation/Core/Setting/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  LastReservation
+//
+//  Created by Berat Yavuz on 21.12.2022.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Calendar")
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
