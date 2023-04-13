7cc275641b722487a8377048e9efce9142a32231
Berat Yavuz <beratttyavuzz@gmail.com>
2023-04-14 02:53:07 +0300

    güncelleme kapasite

--- LastReservation/Core/Main/NewMainMenu/ConfirmationView.swift
+++ LastReservation/Core/Main/NewMainMenu/ConfirmationView.swift
@@ -15,6 +15,11 @@ struct ConfirmationView: View {
     var capacity: Int?
     var db = Firestore.firestore() // Firestore veritabanı referansı
     
+   
+
+
+    
+    
     var body: some View {
         ZStack {
             Color.gray.opacity(0.4)
@@ -38,7 +43,7 @@ struct ConfirmationView: View {
                 HStack {
                     Button(action: {
                         // Onaylama işlemi
-                       
+                       updateCapacity(selectedDay: selectedDay!, selectedSession: selectedSession!)
                         showConfirmation = false
                     }) {
                         Text("Onayla")
@@ -66,12 +71,69 @@ struct ConfirmationView: View {
         }
     }
         
+    func updateCapacity(selectedDay: Days?, selectedSession: String) {
+        if let selectedDay = selectedDay {
+            // Seçili günün timestamp değerini al
+            let timestamp = selectedDay.date
+
+            // Timestamp'i Date veri tipine dönüştür
+            let date = timestamp.dateValue()
+
+            // Seçili günün timestamp değerini milisaniye cinsinden temsil et
+            let timestampInMillis = Int64(date.timeIntervalSince1970 * 1000)
+
+            // Firestore belge yolunu oluştur
+            let documentPath = "Days/\(timestampInMillis)/sessions/\(selectedSession)"
+
+            // Firestore belgesini referansını oluştur
+            let documentRef = db.document(documentPath)
+
+            // Firestore belgesini oku ve güncelleme işlemini gerçekleştir
+            documentRef.getDocument { (snapshot, error) in
+                if let error = error {
+                    // Hata durumunu işle
+                    print("Firestore okuma hatası: \(error.localizedDescription)")
+                } else if let snapshot = snapshot, snapshot.exists {
+                    // Firestore belgesi varsa, mevcut veriyi al
+                    var currentCapacity = snapshot.data()?["capacity"] as? Int ?? 0
+
+                    // Kapasiteyi güncelle
+                    currentCapacity -= 1
+
+                    // Güncellenecek veriyi hazırla
+                    let data: [String: Any] = ["capacity": currentCapacity]
+
+                    // Firestore belgesini güncelle
+                    documentRef.updateData(data) { error in
+                        if let error = error {
+                            // Hata durumunu işle
+                            print("Firestore güncelleme hatası: \(error.localizedDescription)")
+                        } else {
+                            // Başarılı güncelleme durumunu işle
+                            print("Firestore güncelleme başarılı!")
+                            // Güncelleme başarılı olduğunda showConfirmation değerini true yapabilirsiniz.
+                            // Örneğin:
+                            self.showConfirmation = true
+                        }
+                    }
+                } else {
+                    print("Firestore belgesi bulunamadı!")
+                }
+            }
+        }
+    }
+
+
+
+
+
+    
     // Tarih formatını ayarlayan yardımcı fonksiyon
     func formatDate(date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yyyy"
         return dateFormatter.string(from: date)
     }
+
+
 }
-
-
